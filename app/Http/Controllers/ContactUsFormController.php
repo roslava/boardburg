5497acffbb23f20ba4c8768f91c908697440372f
vagrant <vagrant@homestead>
2022-03-08 10:13:16 +0000

    validate

--- app/Http/Controllers/ContactUsFormController.php
+++ app/Http/Controllers/ContactUsFormController.php
@@ -3,13 +3,12 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\ContactUsRequest;
-use Dotenv\Validator;
 use App\Models\Contact;
 
 class ContactUsFormController extends Controller
 {
     // Store Contact Form data
-    public function ContactUsForm(ContactUsRequest $request, Validator $validator): \Illuminate\Http\RedirectResponse
+    public function ContactUsForm(ContactUsRequest $request): \Illuminate\Http\RedirectResponse
     {
         //  Store data in database
         Contact::create($request->all());
@@ -24,11 +23,8 @@ class ContactUsFormController extends Controller
             $message->from('test@artnen.ru');
             $message->to('roslav@icloud.com', 'Admin')->subject($request->get('subject'));
         });
-
-//        $errors = $validator->errors();
             return back()->with('success', 'Мы получили ваше сообщение, и в скором времени свяжемся с вами.');
      }
-
     public function reloadCaptcha()
     {
         return response()->json(['captcha'=> captcha_img()]);
