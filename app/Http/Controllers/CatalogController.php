1d5de2c84dfe5016df03eaffc2c587474bfa9eb6
vagrant <vagrant@homestead>
2022-03-26 00:02:36 +0000

    Catalog/items

--- app/Http/Controllers/CatalogController.php
+++ app/Http/Controllers/CatalogController.php
@@ -7,15 +7,17 @@ use App\Models\Category;
 
 class CatalogController extends Controller
 {
-    public function index(Category $category, Product $product){
+    public function index(Category $category, Product $product)
+    {
         $cat = $category::all();
         $prod = $product::all();
         return compact('cat', 'prod');
     }
 
-    public function show($cat, Category $category ,Product $product){
+    public function show($cat, Category $category, Product $product)
+    {
         $currentCategoryId = $category->query()->where('category_name_en', $cat)->first();
-        $products = $product->query()->where('category_id','=', $currentCategoryId->category_id)->get();
+        $products = $product->query()->where('category_id', '=', $currentCategoryId->category_id)->get();
         return compact('products');
     }
 }
